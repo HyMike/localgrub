e541a13765f1df756208fd9d0a80fc440e97f48d
Michael Hy <hymike88@gmail.com>
2025-05-16 12:38:11 -0700

    creat API to send order data to backend

---
+++ client/src/menu-items.tsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+type MenuItem = {
+    id: number;
+    img: string;
+    dsc: string;
+
+};
+
+const getMenu = async (): Promise<MenuItem[]> => {
+    try {
+        const menu = await axios.get<MenuItem[]>(
+            "https://free-food-menus-api-two.vercel.app/best-foods"
+        );
+        return menu.data;
+    } catch (error) {
+        throw new Error(`Fail to fetch: ${error}`);
+    }
+
+}
+
+export default getMenu;
