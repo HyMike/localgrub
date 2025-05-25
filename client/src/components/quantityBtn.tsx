867e1159378f685f91a3ca88eba94885db8ce76b
Michael Hy <hymike88@gmail.com>
2025-05-25 14:00:45 -0700

    added quantity button to menu and fixed styling with cards

---
+++ client/src/components/quantityBtn.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export const QuantityBtn = () => {
+    const [quantity, SetQuantity] = useState(1);
+
+
+    const handleQuantity = (event: React.MouseEvent<HTMLButtonElement>) => {
+        if (event.currentTarget.textContent === "+") {
+            SetQuantity(quantity + 1);
+        } else {
+            SetQuantity((quantity) => Math.max(1, quantity - 1));
+        }
+    };
+
+    return (
+        <div className="flex items-center justify-around w-full max-w-sm mb-5 mt-3">
+            <label className="text-base leading-none mr-4">Quantity</label>
+
+            <div className="flex items-center space-x-2">
+                <button
+                    className="w-8 h-8 bg-blue-400 text-white rounded-md flex items-center justify-center text-lg"
+                    onClick={handleQuantity}
+                >
+                    -
+                </button>
+                <span className="text-base">{quantity}</span>
+                <button
+                    className="w-8 h-8 bg-blue-400 text-white rounded-md flex items-center justify-center text-lg"
+                    onClick={handleQuantity}
+                >
+                    +
+                </button>
+            </div>
+        </div>
+    );
+
+
+
+
+}
--- client/src/pages/menu.tsx
+++ client/src/pages/menu.tsx
@@ -2,6 +2,8 @@ import { useAuth } from "../authentication/AuthContext";
 import { MenuItems } from "../types/menu";
 import NavBar from "../components/NavBar";
 import SubmitBtn from "../components/SubmitBtn";
+import { ButtonHTMLAttributes, useState } from "react";
+import { QuantityBtn } from "../components/quantityBtn";
 
 
 function MenuPage({ menu }: { menu: MenuItems[] }) {
@@ -15,6 +17,9 @@ function MenuPage({ menu }: { menu: MenuItems[] }) {
                     <li key={id} className="menu-card">
                         <img src={img} width="300" height="300" />
                         <h3>{name}</h3>
+                        {/* <div className="quantity-btn">  */}
+                        <QuantityBtn />
+                        {/* </div> */}
                         <div className="button">
                             <SubmitBtn formData={{ id, name, img }} />
                         </div>
