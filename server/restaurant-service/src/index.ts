38779017dc435182748a05a0add3870acfe791a3
Michael Hy <hymike88@gmail.com>
2025-07-01 17:39:01 -0700

    add payment service container and fix issue with restaurant service database container. Allowing it to run continously

--- server/restaurant-service/src/index.ts
+++ server/restaurant-service/src/index.ts
@@ -4,19 +4,18 @@ import setupDatabase from "./db/setup";
 const app = express();
 app.use(express.json());
 
+// app.get("/", (req, res) => {
+//     res.send("Welcome to deliver app!");
 
+// })
+// app.get("/users", (req, res) => {
+//     res.json([{ id: 1, name: "alice" }]);
+// });
 
-app.get("/", (req, res) => {
-    res.send("Welcome to deliver app!");
-
-})
-app.get("/users", (req, res) => {
-    res.json([{ id: 1, name: "alice" }]);
-});
+setupDatabase();
 
 app.listen(3003, () => {
     console.log("Restaurant service is running on port 3003");
 });
 
-setupDatabase();
 
