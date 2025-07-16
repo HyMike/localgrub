eacf0f245f73804596dbbbe0c18c92e1f53596ff
Michael Hy <hymike88@gmail.com>
2025-07-16 14:40:33 -0700

    removed unused code

--- server/notification-service/src/index.ts
+++ server/notification-service/src/index.ts
@@ -6,10 +6,6 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to deliver app!");
-
-})
-app.get("/users", (req, res) => {
-    res.json([{ id: 1, name: "alice" }]);
 });
 
 app.listen(3001, () => {
